20bc81d2d6878a7be3e75a7e4a1d8c825e08607b
Saurabh Ranjan <26545067+insaurabh@users.noreply.github.com>
2024-01-28 21:29:38 +0530

    crashing on server build

--- src/components/navbar/links/Links.tsx
+++ src/components/navbar/links/Links.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from "react";
 import styles from "./links.module.css";
-import NavLink from "./navLink/navLink";
+import NavLink from "./navLink/navLink.jsx";
 import Image from "next/image";
 // import { handleLogout } from "@/lib/action";
 
